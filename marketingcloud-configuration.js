115958ca6ce33f62b1d67185bcbf1f64e17ab978
eksido <45083828+eksido@users.noreply.github.com>
2023-03-29 11:45:52 +0200

    Bug fixes

--- marketingcloud-configuration.js
+++ marketingcloud-configuration.js
@@ -1,12 +1,12 @@
 module.exports = function(RED) {
     function MarketingcloudConfigurationNode(n) {
-        RED.nodes.createNode(this,n);
-        this.clientid = n.clientid;
-        this.clientsecret = n.clientsecret;
-        this.authenticationurl = n.authenticationurl;
-        this.restbaseurl = n.restbaseurl;
-        this.soapbaseurl = n.soapbaseurl;
-        
+        RED.nodes.createNode(this, n);
+
+        this.clientid = n.clientid || process.env.MARKETINGCLOUD_CLIENT_ID;
+        this.clientsecret = n.clientsecret || process.env.MARKETINGCLOUD_CLIENT_SECRET;
+        this.authenticationurl = n.authenticationurl || process.env.MARKETINGCLOUD_AUTHENTICATION_URL;
+        this.restbaseurl = n.restbaseurl || process.env.MARKETINGCLOUD_RESTBASE_URL;
+        this.soapbaseurl = n.soapbaseurl || process.env.MARKETINGCLOUD_SOAPBASE_URL;
     }
-    RED.nodes.registerType("marketingcloud-configuration",MarketingcloudConfigurationNode);
+    RED.nodes.registerType("marketingcloud-configuration", MarketingcloudConfigurationNode);
 }
